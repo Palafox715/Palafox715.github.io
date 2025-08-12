4669b3ab10d83bb61f4d1b2d1ae876e3d0fb40b8
H.palafox <h.palafox@bdscallcenters.mx>
2025-08-11 21:52:29 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-from tickets_app import app  # <- tu Flask app
+from tickets_app import app  # importa la instancia Flask llamada "app"
